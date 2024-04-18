7c38d0a95e6ee8a85c859f1bd94b64bbe3f5f259
Abigail <abbyg1357@gmail.com>
2024-04-18 11:33:01 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+const hq = 42;
+function distanceFromHqInBlocks(block) {
+    if (block > hq) {
+        return block - hq;
+    }
+    else {
+        return hq - block;
+    }
+}
+
+function distanceFromHqInFeet(block) {
+    distanceFromHqInBlocks(block)
+    return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    const lengthInFeet = 264;
+    if(start > end) {
+        return (start - end) * lengthInFeet;
+    } 
+    else {
+        return (end - start) * lengthInFeet;
+    }
+};
+
+function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end)
+    if (distance < 400) {
+        return 0;
+    }
+    else if ((distance > 400) && (distance <= 2000)) {
+        return ((distance - 400) * 0.02);
+    }
+    else if ((distance > 2000) && (distance <= 2500)) {
+        return 25;
+    }
+
+    else {
+        return 'cannot travel that far'
+    }
+}
